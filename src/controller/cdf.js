0052a251aef779cd7abb5243eab08df35272bbde
huangfugui00 <448438499@qq.com>
2022-03-04 15:40:24 +0800

    push to github

--- src/controller/cdf.js
+++ src/controller/cdf.js
@@ -4,15 +4,10 @@ const Utils = require('../utils/util')
 class CDFSer{
     async readCDF(req,res,next){
         // const cdfObj=new Cdf('static/cdf/6Components_Agilent.CDF')
+        console.log('readCDF')
         const cdfObj=new Cdf('D:\\\\project\\\\js\\\\expressSer-main\\\\src\\\\static\\\\cdf\\\\6Components_Agilent.CDF')
         await cdfObj.readCDF()
-        const data={
-            tics:cdfObj.tics,
-            massArr:cdfObj.massArr,
-            intensityArr:cdfObj.intensityArr,
-            nMass:cdfObj.nMass,
-            scanTimes:cdfObj.scanTimes
-        }
+        const data=cdfObj
         return Utils.responseClient(res,1,200,'移除feeling成功',data);
 
 
--- src/utils/cdf.ts
+++ src/utils/cdf.ts
@@ -1,25 +1,35 @@
 const {readFileSync} = require('fs')
 const {NetCDFReader} = require('netcdfjs')
-
+const math = require('mathjs')
 class Cdf {
     cdfReader
+    mzLen:number=0
+    minMz:number=0
+    maxMz:number=0
+    mzArr:number[]=[]
     scanTimes:number[]=[]
     tics: number[] =[]
     nMass:number =0
     massArr:number[][]=[]
     intensityArr:number[][]=[]
+    alignPeaks:number[][]=[]
     constructor(fileName:string) {
         const file = readFileSync(fileName)
         this.cdfReader = new NetCDFReader(file)
     }
     async readCDF(){
         const mass_values:number[]=this.cdfReader.getDataVariable("mass_values")
+        this.minMz =Math.floor(Math.min(...mass_values))
+        this.maxMz =Math.floor(Math.max(...mass_values))+1
+        this.mzArr = new Array(this.maxMz-this.minMz+1).fill(0).map((x,i)=>i+this.minMz)
+        this.mzLen = this.mzArr.length
         this.tics = this.cdfReader.getDataVariable("total_intensity")
         this.scanTimes=this.cdfReader.getDataVariable('scan_acquisition_time')
         const scan_index:number[] = this.cdfReader.getDataVariable("scan_index")
         const intensity_values:number[] = this.cdfReader.getDataVariable("intensity_values")
         this.nMass = this.tics.length
         await  this.prepareData(mass_values,scan_index,intensity_values)
+        await  this.get3dZ()
     }
 
     async prepareData(mass_values:number[], scan_index:number[], intensity_values:number[]){
@@ -33,6 +43,21 @@ class Cdf {
                 this.intensityArr.push(intensity_values.slice(scan_index[i],scan_index[i+1]))
             }
         }
+    }
+
+    async get3dZ(){
+        // 获得3d矩阵的z
+        for (let i = 0; i < this.nMass; i++) {
+            let singTimePeaks:number[]=new Array(this.mzLen).fill(0);
+            const mass:number[] = this.massArr[i]
+            const intensity:number[] = this.intensityArr[i]
+            for(let j = 0 ;j<this.massArr[i].length;j++){
+                const peakInd = Math.round(mass[j])
+                singTimePeaks[peakInd] = singTimePeaks[peakInd]<intensity[j]?intensity[j]:singTimePeaks[peakInd]
+            }
+            this.alignPeaks.push(singTimePeaks)
+        }
+
     }
 }
 
